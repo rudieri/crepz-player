2762c172194e2719fc6411d9e27b1480e809e4dc
Ararartc <AraraRTC@gmail.com>
2010-08-22 03:15:34 +0000

    O problema do tocando = false não aconteceu aqui! Oo'
    Mas corrigi outros bugs do pause.

---
+++ src/com/configuracao/Centro.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.configuracao;
+
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rudieri
+ */
+public class Centro {
+
+    public Centro(){
+        try {
+            ConfiguracaoSC aaaaaa=new ConfiguracaoSC();
+            aaaaaa.parteChave=null;
+            all = ConfiguracaoBD.listar(aaaaaa);
+        } catch (Exception ex) {
+            Logger.getLogger(Centro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public Object retorna(String chave){
+        return all.get(chave);
+    }
+
+    HashMap<String, String> all;
+}
