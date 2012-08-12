184620a2225550a1ce2b67fa1f8026a35b033445
Ararartc@gmail.com <Ararartc@gmail.com@7d446145-22b0-42ee-b4b3-1d99500bb70f>
2012-08-12 23:40:15 +0000

    teste net

--- trunk/test/Operadores.java
+++ trunk/test/Operadores.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,8 +14,14 @@ public class Operadores {
 
     
     public static void main(String[] args) {
-        System.out.println(2<<2);
-        System.out.println(4>>3);
-        System.out.println(4>>>3);
+        System.out.println(2<<(2^3));
+        System.out.println((true^true));
+        int preto = Color.BLACK.getRGB();
+        int branco = Color.white.getRGB();
+        System.out.println(preto);
+        System.out.println(branco);
+        System.out.println(branco^preto);
+//        System.out.println(4>>3);
+//        System.out.println(4>>>3);
     }
 }
